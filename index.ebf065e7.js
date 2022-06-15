cbcf4c9501f363614f8b8327ad2579a635c29a08
Sl2VA11 <94522796+Sl2VA11@users.noreply.github.com>
2022-06-15 08:51:06 +0000

    Deploying to gh-pages from @ Sl2VA11/goit-js-hw-11@5294a0ed78788ccc396e8bce0fc595735d56135e 🚀

---
+++ index.ebf065e7.js
@@ -0,0 +1,2 @@
+fetch("https://pixabay.com/api/?key=28055837-436de9124f9241a9654f163e0&q=cat&image_type=photo&pretty=true").then((function(t){return t.json()})).then(console.log);
+//# sourceMappingURL=index.ebf065e7.js.map
