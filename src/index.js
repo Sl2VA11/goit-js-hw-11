44625c2ee5c6c1ee00ac7779780a722fd0ded453
Slava Grobovoy <slava.grobovoy27@ukr.net>
2022-06-15 21:21:08 +0300

    add photo card

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
-fetch('https://pixabay.com/api/?key=28055837-436de9124f9241a9654f163e0&q=cat&image_type=photo&pretty=true')
+
+
+import photoCardMarkup from './js/photo-card-markup'
+
+
+
+
+export const refs = {
+   searchForm: document.querySelector('#search-form'),
+   searchBtn: document.querySelector('.search-btn'),
+   divGallery: document.querySelector('.gallery'),
+}
+
+refs.searchForm.addEventListener('submit', onSearch)
+
+
+
+function onSearch(e) {
+   e.preventDefault()
+
+  const searchQuery = e.currentTarget.elements.searchQuery.value
+   
+   
+   fetch(`https://pixabay.com/api/?key=28055837-436de9124f9241a9654f163e0&q=${searchQuery}&image_type=photo&safesearch=true&orientation=horizontal&per_page=40&page=1`)
    .then(response => response.json())
-   .then(console.log)
+      .then(hits => {
+         
+         photoCardMarkup(hits)
+      
+   })
+   
+}
+
+
+
+
+
+
---
+++ src/js/photo-card-markup.js
@@ -0,0 +1,39 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { refs } from '../index.js'
+
+const lightbox = new SimpleLightbox('.gallery a', { captionData: 'alt', captionDelay: 250, });
+
+export default function addPhotoCardMarkup(hits) {
+  
+   refs.divGallery.insertAdjacentHTML('beforeend', makePhotoCardMarkup(hits))
+   lightbox.refresh()
+}
+
+function makePhotoCardMarkup({hits}) {
+   console.log(hits);
+    
+   return hits.map(
+    ({ largeImageURL, webformatURL, tags, likes, views,comments, downloads }) =>
+      `<div class="photo-card">
+         <a class="gallery-item" href="${largeImageURL}">
+            <img class="gallery-image" src="${webformatURL}" alt="${tags}" loading="lazy" />
+         </a>
+         <div class="info">
+            <p class="info-item">
+               <b>Likes: </b></br>${likes}
+            </p>
+            <p class="info-item">
+               <b>Views: </b></br>${views}
+            </p>
+            <p class="info-item">
+               <b>Comments: </b></br>${comments}
+            </p>
+            <p class="info-item">
+               <b>Downloads: </b></br>${downloads}
+            </p>
+         </div>
+    </div>`
+  ).join('')
+  
+}
